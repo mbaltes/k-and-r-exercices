41cc757a21406f024ad8c5c211a672d8211c6aa4
Mark Baltes <mark@mcb.me>
2018-01-04 09:37:34 -0800

    Exercises 1-5 through 1-7.

---
+++ exer1-6.c
@@ -0,0 +1,9 @@
+// Verify getchar() != EOF is 0 or 1
+
+#include <stdio.h>
+
+int main() {
+  int c;
+  c = getchar() != EOF; // Ctrl-D is EOF input, makes c = 0
+  printf("%i\n", c);
+}
---
+++ exer1-7.c
@@ -0,0 +1,7 @@
+// Print the value of EOF
+
+#include <stdio.h>
+
+int main() {
+  printf("%i\n", EOF); // EOF = -1
+}
