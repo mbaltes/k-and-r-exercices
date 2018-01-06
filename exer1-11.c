093b342aa69665876e1ad17081ef78d3e490055b
Mark Baltes <mark@mcb.me>
2018-01-05 21:17:45 -0800

    Exercise 1-11.

---
+++ exer1-11.c
@@ -0,0 +1,27 @@
+// Testing the word count program.
+
+#include <stdio.h>
+
+#define IN  1 // Inside a word.
+#define OUT 0 // Outside a word.
+
+int main() {
+  int c, nl, nw, nc, state;
+
+  state = OUT;
+  nl = nw = nc = 0;
+  while ((c = getchar()) != EOF) {
+    ++nc;
+    if (c == '\n') {
+      ++nl;
+    }
+    if (c == ' ' || c == '\n' || c == '\t') {
+      state = OUT;
+    } else if (state == OUT) {
+      state = IN;
+      ++nw;
+    }
+  }
+  printf("\n");
+  printf("%d %d %d\n", nl, nw, nc);
+}
