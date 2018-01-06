efcac141fa822aeaf1bd630fd33fafac1c106898
Mark Baltes <mark@mcb.me>
2018-01-06 10:46:19 -0800

    Exercise 1-12.

---
+++ exer1-12.c
@@ -0,0 +1,15 @@
+// This program prints its input one word per line.
+
+#include <stdio.h>
+
+int main() {
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      printf("\n");
+      c = 0;
+    }
+    putchar(c);
+  }
+}
