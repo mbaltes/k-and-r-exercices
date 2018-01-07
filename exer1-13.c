a36decf6f242ec56493bc89e554a5ef90ab62362
Mark Baltes <mark@mcb.me>
2018-01-06 19:40:36 -0800

    Exercise 1-13.

---
+++ exer1-13.c
@@ -0,0 +1,23 @@
+// This program prints a histogram of the lengths of the words
+// in its input.
+
+#include <stdio.h>
+
+int main() {
+  int c, nword, word_length;
+
+  nword = word_length = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n') {
+      ++nword;
+      printf("%2d ", nword);
+      for (int i = 0; i < word_length; ++i) {
+        printf("|");
+      }
+      word_length = 0;
+      printf("\n");
+    } else {
+      ++word_length;
+    }
+  }
+}
