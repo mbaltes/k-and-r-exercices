62489d88b2b7220bf64d79d5dc4c7fa29a2867f5
Mark Baltes <mark@mcb.me>
2018-07-07 19:37:52 -0700

    Added exercise 1-14.

--- exer1-14.c
+++ exer1-14.c
@@ -6,7 +6,35 @@
  */
 #include <stdio.h>
 
+#define SIZE 128
+
+
+void print_histogram(int length);
+
+
 int main()
 {
-	// TODO
+	int c;
+	static int freq[SIZE];
+
+	while ((c = getchar()) != EOF) {
+		freq[(int)c]++;
+	}
+
+	printf("\n");
+	for (int i = 0; i < SIZE; ++i) {
+		if (freq[i] != 0) {
+			printf("%c ", (char)i);
+			print_histogram(freq[i]);
+		}
+	}
+}
+
+
+void print_histogram(int length)
+{
+	for (int i = 0; i < length; ++i) {
+		printf("|");
+	}
+	printf("\n");
 }
